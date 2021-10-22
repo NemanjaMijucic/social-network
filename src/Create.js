7d62ed202961287b3d0af659d16fab670097a45a
NemanjaMijucic <nemanja.mijuci@gmail.com>
2021-10-22 12:13:26 +0200

    deploy

--- src/Create.js
+++ src/Create.js
@@ -5,10 +5,11 @@ import './download.png'
 
 
 function Create() {
+  
 
   const [enteredName, setEnteredName] = useState('');
   const [enteredLastName, setEnteredLastName] = useState('');
-  const [enteredTags, setEnteredTags] = useState('');
+  const [enteredPostTag, setEnteredPostTag] = useState('');
   const [enteredComment, setEnteredComment] = useState('');
 
 
@@ -21,39 +22,58 @@ function Create() {
   }
 
   const tagsChangeHandler =(event) => {
-      setEnteredTags(event)
+      setEnteredPostTag(event)
   }
 
   const commentChangeHandler = (event) => {
       setEnteredComment(event.target.value);
   }
 
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const postData = {
+      name: enteredName,
+      lastName: enteredLastName,
+      tags: enteredPostTag,
+      comment: enteredComment
+    }
+
+    console.log(postData);
+    setEnteredName('');
+    setEnteredLastName('');
+    setEnteredPostTag('');
+    setEnteredComment('');
+
+  }
 
   return (
     <div className="create">
-      <form>
+      <form onSubmit={submitHandler}>
         <div className="post-form">
-        <label>Name</label>
-        <input type="text" placeholder="name" onChange={nameChangeHandler}></input>
+        <label htmlFor='name'>Name</label>
+        <input type="text" placeholder="name" onChange={nameChangeHandler} value={enteredName}></input>
         </div>
         <div className="post-form">
-        <label>Last Name</label>
-        <input type="text" placeholder="last name" onChange={lastNameChangeHandler}></input>
+        <label htmlFor='last name'>Last Name</label>
+        <input type="text" placeholder="last name" onChange={lastNameChangeHandler} value={enteredLastName}></input>
         </div>
         <div className="post-form">
-        <label>Tags</label>
-        <input type="text" placeholder="tags" onChange={tagsChangeHandler}></input>
+        <label htmlFor='tags'>Tags</label>
+        <input type="text" placeholder="tags" onChange={tagsChangeHandler} value={enteredPostTag}></input>
         </div>
         <div className="post-form">
-        <label>Comment</label>
-        <input type="text" placeholder="comment" onChange={commentChangeHandler}></input>
+        <label htmlFor='comment'>Comment</label>
+        <input type="text" placeholder="comment" onChange={commentChangeHandler} value={enteredComment}></input>
         </div>
         <div className="post-form">
-        <label>Image</label>
-        <input type="file" width="80" height="55" />
+        <label htmlFor='file'>Image</label>
+        <input type="file" width="80" height="55" name='file'/>
         </div>
-        
-     
+        <div className='post-form'>
+        <button type='submit' className='btn-submit'>add post</button>
+        </div>
+   
     
       </form>
     </div>
